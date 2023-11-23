04a9be4e0863cd3d0e47f215ca3bbd8cd0c8bc55
Gabriel Gonçalves de Sousa <gabriel.sousa@duosystem.com.br>
2023-11-23 14:50:31 -0300

    Batatinha

--- src/Register/index.tsx
+++ src/Register/index.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 export const Register = () => {
+  const navigate = useNavigate()
   return (
     <>
       <h1>Register</h1>
+
+      <button onClick={() => navigate(-1)}>Back</button>
     </>
-  );
-};
+  )
+}
--- src/Update/index.tsx
+++ src/Update/index.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 export const Update = () => {
+  const navigate = useNavigate()
   return (
     <>
       <h1>Update</h1>
+      <button onClick={() => navigate(-1)}>Back</button>
     </>
-  );
-};
+  )
+}
